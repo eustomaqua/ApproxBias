3d23ee54bb82c2c5c156097e6896cd038dbd7e0a
eustomaqua <yjbian92@gmail.com>
2026-05-17 16:08:57 +0200

    almost hyper_pms drawing

--- experiment/df_zip/cont_draw.py
+++ experiment/df_zip/cont_draw.py
@@ -5,17 +5,97 @@ import numpy as np
 import matplotlib.pyplot as plt
 # from matplotlib.gridspec import GridSpecFromSubplotSpec
 # from mpl_toolkits.axes_grid1 import ImageGrid
+from hfm.hfm_df import differentiate_tim, differentiate_val
 
 from pyfair.facil.utils_const import DTY_FLT, subfig_ind
 from pyfair.facil.draw_prelim import _style_set_axis, _setup_figshow
 from pyfair.granite.draw_graph import _sns_line_err_bars
+from pyfair.granite.draw_addtl import (
+    _subproc_pl_lin_reg, _subproc_pl_lin_reg_alt, _subproc_pl_identity)
 
 
+# plt.rcParams["text.usetex"] = True
 _pl_myclr = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
              '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 _navy = 'navy'
 
 
-def hyper_pms_lin_reg_gather():
-    fig = plt.figure(figsize=(14, 7.8), dpi=300, constrained_layout=True)
+def _hp_subproc_linreg(ax, X, Ys, picked_keys, annotZ, snspec, myclr,
+                       corr=False, curr_legend_nb_split=4, identity=True,
+                       differentiate=False):
+    _curr_mk = ['D', '^', 'v', 'o', '*', 's', 'd', '<', '>']
+    _curr_sz = [10, 15, 15, 12, 29, 11, 15, 15, 15]
+    if isinstance(picked_keys, str):
+        _subproc_pl_lin_reg(ax, X, Ys, picked_keys, annotZ, snspec, _navy,
+                            False, corr, 's', 11)
+        _subproc_pl_lin_reg_alt(ax, X, Ys, snspec, _navy)
+    else:
+        for i, key in enumerate(picked_keys):
+            _subproc_pl_lin_reg(ax, X, Ys[i], key, annotZ, snspec, myclr[i],
+                                True, corr, _curr_sz[i], _curr_mk[i])
+        for i, key in enumerate(picked_keys):
+            _subproc_pl_lin_reg_alt(ax, X, Ys[i], snspec, myclr[i])
+    if identity:
+        _subproc_pl_identity(ax, [X, X], annotZ, snspec)
+
+    if snspec in ['sty5a', 'sty5b', 'sty6']:
+        _curr_nc = 1 if len(picked_keys) <= curr_legend_nb_split else 2
+        _curr_lo = 'upper right' if snspec == 'sty6' else 'upper left'
+        _curr_fram = dict(frameon=True, loc=_curr_lo, framealpha=.5,
+                          handleheight=1.2, handletextpad=0,
+                          ncol=_curr_nc, columnspacing=.8)
+        del _curr_nc, _curr_lo
+    elif snspec in ['sty3a', 'sty3b', 'sty8a', 'sty8b']:
+        _curr_fram = dict(frameon=False, loc='upper left')
+    elif snspec in ['sty4', ]:  # 'sty6', 'sty4']:
+        _curr_fram = dict(frameon=True, framealpha=.5, loc='best')
+        #                   # loc='upper right' if snspec == 'sty6' else 'best')
+    _curr_ft = plt.rcParams['font.family']  # 'Times New Roman'
+    legend_font = dict(family=_curr_ft, size=8)
+    ax.legend(prop=legend_font, labelspacing=.35, **_curr_fram)
+    del _curr_fram, _curr_ft, _curr_sz, _curr_mk
+
+    ax = _style_set_axis(ax, invt=False, dirc='in')
+    # ax.set_xlabel(annots[0], fontsize=9, family=_curr_ft, x=.55)
+    # ax.set_ylabel(annots[1], fontsize=9, family=_curr_ft, y=.55)
+    return
+
+
+def hyper_pms_lin_reg_gather(X, Ys, picked_keys, annots=('X', 'Y', 'Y=X'),
+                             figname='smd', sharey=True, strt=0, snspec='sty',
+                             identity=True, differentiate=False):
+    num_c = len(X)  # =len(Ys)=len(picked_keys)  # figsize=(14, 2.8)
+    fig = plt.figure(figsize=(9.1, 2.4) if num_c > 2 else (4.34, 2.4),
+                     dpi=300, constrained_layout=True)
+    outer = fig.add_gridspec(1, num_c, wspace=.07, hspace=.35)
+    annotZ = annots[2] if len(annots) > 2 else r'$f(x)=x$'
+    corr = not ('tim' in figname)
+    ref_ax = None
+    for ik, ax_spec in enumerate(outer):
+        if sharey:
+            inner = fig.add_subplot(ax_spec, sharey=ref_ax)  # if sharey else None)
+            if ref_ax is None:
+                ref_ax = inner
+        else:
+            inner = fig.add_subplot(ax_spec)
+        curr_X, curr_Ys = X[ik], Ys[ik]
+        if differentiate and corr:
+            curr_Ys = [differentiate_val(curr_X, curY) for curY in curr_Ys]
+        elif differentiate:
+            curr_Ys = [differentiate_tim(curr_X, curY) for curY in curr_Ys]
+        _hp_subproc_linreg(inner, curr_X, curr_Ys, picked_keys[ik], annotZ, snspec,
+                           _pl_myclr, corr, curr_legend_nb_split=(6 if corr else 4),
+                           identity=identity, differentiate=differentiate)
+        # 只让最左边显示 y 轴
+        if ik > 0:
+            inner.tick_params(labelleft=False)   # 隐藏 y tick label
+            inner.set_ylabel("")                 # 隐藏 y label
+        else:
+            inner.set_ylabel(annots[1], fontsize=9)  # ,y=.45)
+            tmp_y = inner.get_ylim()
+            if tmp_y[1] // np.max(Ys) > 4:
+                inner.set_ylim((min(tmp_y[0] + 10, 0.), tmp_y[1] / 4))
+            del tmp_y
+        inner.set_xlabel(annots[0] + f'\n{subfig_ind(ik+strt)}', fontsize=9)  # ,x=.45)
+    _setup_figshow(fig, figname)
     return
--- experiment/df_zip/mcvg_plt.py
+++ experiment/df_zip/mcvg_plt.py
@@ -14,6 +14,7 @@ from experiment.df_nonbin.rev_mext_plt_cor import (
 
 from pyfair.granite.draw_addtl import (
     hyper_params_lin_reg, multi_lin_reg_without_distr)
+from experiment.df_zip.cont_draw import hyper_pms_lin_reg_gather
 
 
 class GraphSetup(GraphSetupVer2):
@@ -137,29 +138,35 @@ class DistPerf_draw(GraphSetup):
                 annotX = r'T_{ \mathbf{D}_{a}(S,a_i) }'
                 annotY = r'T_{ \hat{\mathbf{D}}_a(S,a_i) }'
                 anotAP = r'\lg(\frac{ T_{\hat{\mathbf{D}}_a(S,a_i)} }{ T_{\mathbf{D}_a(S,a_i)} })'
+                anotID = r'T_{ \hat{\mathbf{D}}_a }=T_{ \mathbf{D}_a }'
             elif rmk.endswith('avg'):
                 annotX = r'\mathbf{D}_a^\text{avg}(S,a_i)'
                 annotY = r'\hat{\mathbf{D}}_a^\text{avg}(S,a_i)'
                 anotAP = r'\frac{ \hat{\mathbf{D}}_a^\text{avg}(S,a_i) }{ \mathbf{D}_a^\text{avg}(S,a_i) }-1'
+                anotID = r'\hat{\mathbf{D}}_a^\text{avg} = \mathbf{D}_a^\text{avg}'
             else:
                 annotX = r'\mathbf{D}_a(S,a_i)'
                 annotY = r'\hat{\mathbf{D}}_a(S,a_i)'
                 anotAP = r'\frac{ \hat{\mathbf{D}}_a(S,a_i) }{ \mathbf{D}_a(S,a_i) }-1'
-            return annotX, annotY, anotAP
+                anotID = r'\hat{\mathbf{D}}_a = \mathbf{D}_a'
+            return annotX, annotY, anotAP, anotID
 
         if rmk.endswith('tim'):
             annotX = r'T_{ \mathbf{D}_{\mathbf{a}}(S) }'
             annotY = r'T_{ \hat{\mathbf{D}}_{\mathbf{a}}(S) }'
             anotAP = r'\lg(\frac{ T_{\hat{\mathbf{D}}_{\mathbf{a}}(S)} }{ T_{\mathbf{D}_{\mathbf{a}}(S)} })'
+            anotID = r'T_{ \hat{\mathbf{D}}_{\mathbf{a}} }=T_{ \mathbf{D}_{\mathbf{a}} }'
         elif rmk.endswith('avg'):
             annotX = r'\mathbf{D}_{\mathbf{a}}^\text{avg}(S)'
             annotY = r'\hat{\mathbf{D}}_{\mathbf{a}}^\text{avg}(S)'
             anotAP = r'\frac{ \hat{\mathbf{D}}_{\mathbf{a}}^\text{avg}(S) }{ \mathbf{D}_{\mathbf{a}}^\text{avg}(S) }-1'
+            anotID = r'\hat{\mathbf{D}}_{\mathbf{a}}^\text{avg} = \mathbf{D}_{\mathbf{a}}^\text{avg}'
         else:
             annotX = r'\mathbf{D}_{\mathbf{a}}(S)'
             annotY = r'\hat{\mathbf{D}}_{\mathbf{a}}(S)'
             anotAP = r'\frac{ \hat{\mathbf{D}}_{\mathbf{a}}(S) }{ \mathbf{D}_{\mathbf{a}}(S) }-1'
-        return annotX, annotY, anotAP
+            anotID = r'\hat{\mathbf{D}}_{\mathbf{a}} = \mathbf{D}_{\mathbf{a}}'
+        return annotX, annotY, anotAP, anotID
 
 
 class cvgPlt1_anal_gather(DistPerf_draw):
@@ -167,13 +174,147 @@ class cvgPlt1_anal_gather(DistPerf_draw):
     _m1_set = list(range(3, 34, 2))  # len=16
 
     def schedule_mspaint(self, raw_df_mA, raw_df_mB, pre='minmax'):
-        ma_max, ma_avg, ma_tim, ma_pick = self.prepare_graph(len(self._m2_set))
+        ma_n_l, mb_n_l = len(self._m2_set), len(self._m1_set)
+        ma_max, ma_avg, ma_tim, ma_pick = self.prepare_graph(ma_n_l)
         ma_picked_set = [r'$m_2$={}'.format(self._m2_set[i]) for i in ma_pick]
-        mb_max, mb_avg, mb_tim, mb_pick = self.prepare_graph(len(self._m1_set))
+        mb_max, mb_avg, mb_tim, mb_pick = self.prepare_graph(mb_n_l)
         mb_picked_set = [r'$m_1$={}'.format(self._m1_set[i]) for i in mb_pick]
         id_set = self.recap_sub_data(raw_df_mA, nb_row=4, nc_norm=6, nc_sens=0)[1]
         id_set = self.recap_sub_data(raw_df_mB, nb_row=4, nc_norm=6, nc_sens=0)[1]
-        pdb.set_trace()
+        n_l = {'ma': ma_n_l, 'mb': mb_n_l}
+        picked_m = {'ma': ma_pick, 'mb': mb_pick}
+        tag_d_max = {'ma': ma_max, 'mb': mb_max}
+        tag_d_avg = {'ma': ma_avg, 'mb': mb_avg}
+        tag_tim = {'ma': ma_tim, 'mb': mb_tim}
+        picked_set = {'ma': ma_picked_set, 'mb': mb_picked_set}
+        del ma_n_l, ma_max, ma_avg, ma_tim, ma_pick, ma_picked_set
+        del mb_n_l, mb_max, mb_avg, mb_tim, mb_pick, mb_picked_set
+        raw_df = {'ma': raw_df_mA, 'mb': raw_df_mB}
+
+        pos = 2
+        fgn = f'{self._figname}_{pre}_pos{pos}_multivar'
+        self.present_multivar_gather(
+            n_l, tag_tim, picked_m, picked_set, fgn, raw_df, id_set, pos, 'tim')
+        self.present_multivar_gather(
+            n_l, tag_d_max, picked_m, picked_set, fgn, raw_df, id_set, pos, 'dmax')
+        self.present_multivar_gather(
+            n_l, tag_d_avg, picked_m, picked_set, fgn, raw_df, id_set, pos, 'davg')
+
+        fgn = fgn.replace('multivar', 'sasing')  # 'sa_non')
+        self.present_sa_sing_gather(
+            n_l, tag_tim, picked_m, picked_set, fgn, raw_df, id_set, pos, 'tim')
+        self.present_sa_sing_gather(
+            n_l, tag_d_max, picked_m, picked_set, fgn, raw_df, id_set, pos, 'dmax')
+        self.present_sa_sing_gather(
+            n_l, tag_d_avg, picked_m, picked_set, fgn, raw_df, id_set, pos, 'davg')
+        return
+
+    # def present_multivar_gather(self, ma_tags, ma_picked, mb_tags, mb_picked,
+    #                             fgn, interm=False):  # intermediate=
+    #     return
+    def present_multivar_gather(self, n_l, tags, picked_m, picked_set, fgn,
+                                dframe, id_set, pos, rmk):
+        tag_ma = self.incise_graph(tags['ma'][0], n_l['ma'], picked_m['ma'])
+        tag_mb = self.incise_graph(tags['mb'][0], n_l['mb'], picked_m['mb'])
+        tag_ma = tag_ma[0] + tag_ma[1] + tag_ma[2] + tag_ma[3]
+        tag_mb = tag_mb[0] + tag_mb[1] + tag_mb[2] + tag_mb[3]
+        df_ma = self.obtn_sa_multivar(dframe['ma'], id_set, pos, tag_ma, [])
+        df_mb = self.obtn_sa_multivar(dframe['mb'], id_set, pos, tag_mb, [])
+
+        tX_a = df_ma[tag_ma[0]].values.astype(DTY_FLT)
+        tX_b = df_mb[tag_mb[0]].values.astype(DTY_FLT)
+        tt = 5 + 1
+        antX, antY, _, antZ = self.quick_rmk(rmk, interm=False)  # ,antAP
+        annots = ['${}$ (sec)'.format(antX), '${}$ (sec)'.format(
+            antY), '${}$'.format(antZ)]  # '${} = {}$'.format(antY, antX)]
+        strt = 8 if rmk == 'tim' else (4 if rmk.endswith('avg') else 0)
+        hyper_pms_lin_reg_gather([tX_a, tX_a, tX_b, tX_b], [
+            df_ma[tag_ma[1:tt]].values.astype(DTY_FLT).T,
+            df_ma[tag_ma[-5:]].values.astype(DTY_FLT).T,
+            df_mb[tag_mb[1:tt]].values.astype(DTY_FLT).T,
+            df_mb[tag_mb[-5:]].values.astype(DTY_FLT).T, ], [
+            picked_set['ma'], picked_set['ma'], picked_set['mb'], picked_set['mb']],
+            annots, f'{fgn}_{rmk}', strt=strt, snspec='sty5b',
+        )  # snspec='sty6', differentiate=True)
+
+        # strt = 4 if rmk == 'tim' else (2 if rmk.endswith('avg') else 0)
+        # hyper_pms_lin_reg_gather([tX_a, tX_b], [
+        #     df_ma[tag_ma[tt]].values.astype(DTY_FLT).T,
+        #     df_mb[tag_mb[tt]].values.astype(DTY_FLT).T, ],
+        #     ['', ''], annots, f'{fgn}_my_{rmk}', strt=strt,
+        #     # snspec='sty3{}'.format('b' if rmk != 'tim' else 'a'))
+        #     snspec=('sty8b' if rmk != 'tim' else 'sty8a'))
+        return
+
+    def present_sa_sing_gather(self, n_l, tags, picked_m, picked_set, fgn,
+                               dframe, id_set, pos, rmk, sa_bin=False):
+        tag_sa = {}
+        tag_sa1 = self.incise_graph(tags['ma'][1], n_l['ma'], picked_m['ma'])
+        tag_sa2 = self.incise_graph(tags['ma'][2], n_l['ma'], picked_m['ma'])
+        tag_sa1 = tag_sa1[0] + tag_sa1[1] + tag_sa1[2] + tag_sa1[3]
+        tag_sa2 = tag_sa2[0] + tag_sa2[1] + tag_sa2[2] + tag_sa2[3]
+        df_ma = self.obtn_sa_multivar(dframe['ma'], id_set, pos, tag_sa1, tag_sa2)
+        tag_sa['ma_sa1'] = tag_sa1
+        tag_sa['ma_sa2'] = tag_sa2
+        tag_sa1 = self.incise_graph(tags['mb'][1], n_l['mb'], picked_m['mb'])
+        tag_sa2 = self.incise_graph(tags['mb'][2], n_l['mb'], picked_m['mb'])
+        tag_sa1 = tag_sa1[0] + tag_sa1[1] + tag_sa1[2] + tag_sa1[3]
+        tag_sa2 = tag_sa2[0] + tag_sa2[1] + tag_sa2[2] + tag_sa2[3]
+        df_mb = self.obtn_sa_multivar(dframe['mb'], id_set, pos, tag_sa1, tag_sa2)
+        tag_sa['mb_sa1'] = tag_sa1
+        tag_sa['mb_sa2'] = tag_sa2
+
+        tX_a = df_ma[tag_sa['ma_sa1'][0]].values.astype(DTY_FLT)
+        tX_b = df_mb[tag_sa['mb_sa1'][0]].values.astype(DTY_FLT)
+        tt = 5 + 1
+        antX, antY, antY_diff, antZ = self.quick_rmk(rmk, interm=True)
+        annots = ['${}$ (sec)'.format(antX), '${}$ (sec)'.format(
+            antY), '${}$'.format(antZ)]  # '${} = {}$'.format(antY, antX)]
+        strt = 8 if rmk == 'tim' else (4 if rmk.endswith('avg') else 0)
+        curr_Ys = [df_ma[tag_sa['ma_sa1'][1:tt]].values.astype(DTY_FLT).T,
+                   df_ma[tag_sa['ma_sa1'][-5:]].values.astype(DTY_FLT).T,
+                   df_mb[tag_sa['mb_sa1'][1:tt]].values.astype(DTY_FLT).T,
+                   df_mb[tag_sa['mb_sa1'][-5:]].values.astype(DTY_FLT).T, ]
+        curr_Zs = [picked_set['ma'], picked_set['ma'], picked_set['mb'], picked_set['mb']]
+        hyper_pms_lin_reg_gather([tX_a, tX_a, tX_b, tX_b], curr_Ys, curr_Zs,
+                                 annots, f'{fgn}_{rmk}', strt=strt, snspec='sty5b')
+        # annots[1] = '${}$'.format(antY_diff)
+        # hyper_pms_lin_reg_gather([tX_a, tX_a, tX_b, tX_b], curr_Ys, curr_Zs,
+        #                          annots, f'{fgn}_{rmk}6', strt=strt,  # snspec='sty5b',
+        #                          snspec='sty6', differentiate=True)
+        # annots[1] = '${}$ (sec)'.format(antY)
+
+        if not sa_bin:
+            return
+        fgn = fgn.replace('sasing', 'sa_non')
+        df_ma = self.obtn_sa_nonbin(
+            dframe['ma'], id_set, pos, tag_sa['ma_sa1'], tag_sa['ma_sa2'])
+        df_mb = self.obtn_sa_nonbin(
+            dframe['mb'], id_set, pos, tag_sa['mb_sa1'], tag_sa['mb_sa2'])
+        tX_a = df_ma[tag_sa['ma_sa1'][0]].values.astype(DTY_FLT)
+        tX_b = df_mb[tag_sa['mb_sa1'][0]].values.astype(DTY_FLT)
+        hyper_pms_lin_reg_gather([tX_a, tX_a, tX_b, tX_b], [
+            df_ma[tag_sa['ma_sa1'][1:tt]].values.astype(DTY_FLT).T,
+            df_ma[tag_sa['ma_sa1'][-5:]].values.astype(DTY_FLT).T,
+            df_mb[tag_sa['mb_sa1'][1:tt]].values.astype(DTY_FLT).T,
+            df_mb[tag_sa['mb_sa1'][-5:]].values.astype(DTY_FLT).T, ], [
+            picked_set['ma'], picked_set['ma'], picked_set['mb'], picked_set['mb']],
+            annots, f'{fgn}_{rmk}', strt=strt, snspec='sty5b')
+
+        fgn = fgn.replace('sa_non', 'sa_bin')  # 'sasing'
+        df_ma = self.obtn_sa_bin(
+            dframe['ma'], id_set, pos, tag_sa['ma_sa1'], tag_sa['ma_sa2'])
+        df_mb = self.obtn_sa_bin(
+            dframe['mb'], id_set, pos, tag_sa['mb_sa1'], tag_sa['mb_sa2'])
+        tX_a = df_ma[tag_sa['ma_sa1'][0]].values.astype(DTY_FLT)
+        tX_b = df_mb[tag_sa['mb_sa1'][0]].values.astype(DTY_FLT)
+        hyper_pms_lin_reg_gather([tX_a, tX_a, tX_b, tX_b], [
+            df_ma[tag_sa['ma_sa1'][1:tt]].values.astype(DTY_FLT).T,
+            df_ma[tag_sa['ma_sa1'][-5:]].values.astype(DTY_FLT).T,
+            df_mb[tag_sa['mb_sa1'][1:tt]].values.astype(DTY_FLT).T,
+            df_mb[tag_sa['mb_sa1'][-5:]].values.astype(DTY_FLT).T, ], [
+            picked_set['ma'], picked_set['ma'], picked_set['mb'], picked_set['mb']],
+            annots, f'{fgn}_{rmk}', strt=strt, snspec='sty5b')
         return
 
 
